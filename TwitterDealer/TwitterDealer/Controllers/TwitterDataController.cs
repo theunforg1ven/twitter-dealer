a099e53a74fb601c2d80c5cd13c35e938283d448
TheUnforg1ven <tbatjuk4u@gmail.com>
2019-11-02 09:01:07 +0200

    Created new method for getting all tweet info

---
+++ TwitterDealer/TwitterDealer/Controllers/TwitterDataController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TweetSharp;
+using TwitterDealer.Interfaces;
+
+namespace TwitterDealer.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class TwitterDataController : ControllerBase
+	{
+		private readonly ITweetDataService _tweetDataService;
+
+		public TwitterDataController(ITweetDataService tweetDataService)
+		{
+			_tweetDataService = tweetDataService;
+		}
+
+		[HttpGet]
+		[Route("TwitterData")]
+		public async Task<TwitterStatus> GetUserTweetsAsync(string tweetUrl)
+		{
+			var infoResult = await _tweetDataService.GetUserTweetsAsync(tweetUrl);
+
+			return infoResult;
+		}
+	}
+}
