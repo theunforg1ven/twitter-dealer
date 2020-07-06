3dada1bfb819b2983521fa56759e51be6d768acc
TheUnforg1ven <tbatjuk4u@gmail.com>
2020-07-06 21:15:49 +0300

    Updated frontend app

--- TwitterDealer/TwitterDealer.WebClientApp/ClientApp/README.md
+++ TwitterDealer/TwitterDealer.WebClientApp/ClientApp/README.md
@@ -1,6 +1,6 @@
 # ClientApp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.9.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.1.
 
 ## Development server
 
--- TwitterDealer/TwitterDealer.WebClientApp/ClientApp/src/app/app-routing.module.ts
+++ TwitterDealer/TwitterDealer.WebClientApp/ClientApp/src/app/app-routing.module.ts
@@ -1,26 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UserComponent } from './user/user.component';
-import { RegistrationComponent } from './user/registration/registration.component';
-import { LoginComponent } from './user/login/login.component';
-import { HomeComponent } from './home/home.component';
-import { AuthGuard } from './auth/auth.guard';
-import { UserprofileComponent } from './home/userprofile/userprofile.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/user/registration', pathMatch: 'full' },
-  { path: 'user', component: UserComponent,
-  children: [
-    { path: 'registration', component: RegistrationComponent },
-    { path: 'login', component: LoginComponent }
-    ]
-  },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard],
-  children: [
-    { path: 'userprofile', component: UserprofileComponent },
-    ]
-  },
-];
+
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
